a3c5a01917c6d5c96767f24c1bbeb8e13fe92f40
Hudha11 <miftakhul.hudha11@gmail.com>
2025-12-07 05:29:36 +0800

    feat: config livewire

---
+++ app/Livewire/Manager/Approval/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Manager\Approval;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.manager.approval.index');
+    }
+}
